628690e16ede52cd44fbc284ea47eaba50ccb640
Sharmeen Khan <sharmeenkhan787787@gmail.com>
2026-07-09 19:51:50 +0530

    Practiced lists and tuples

---
+++ chapter4 PS (Lists and Tuples)/problem3.py
@@ -0,0 +1,4 @@
+# Check that tuple type cannot be changed in python
+
+a=(55,"sharmeen")
+a[0]=33  #cannot be changed, will show error
---
+++ chapter4 PS (Lists and Tuples)/problem4.py
@@ -0,0 +1,4 @@
+# WAP to sum a list with 4 numbers
+
+numbers=[65,88,66,97]
+print(sum(numbers))
